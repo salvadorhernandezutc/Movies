097659a1abd330f22975bec7a53670a9f271d330
Ing. Salvador Alejandro Hernandez Cepeda <salvadorh@utc.edu.mx>
2025-11-10 17:07:17 -0600

    Update

--- dashboard.php
+++ dashboard.php
@@ -3,15 +3,15 @@
     include 'php/session.php';
 ?>
 <!DOCTYPE html>
-<html lang="es" data-mdb-theme="light">
+<html lang="es" data-mdb-theme="dark">
     <head>
         <?php headContent("Administracion de Peliculas"); ?>
     </head>
     <body>
-    
         <h1>Administrar Peluclas</h1>
         <h2>Bienvenido <?php echo $_SESSION['username'] ?></h2>
-
+        <h2><?php echo $_SESSION['fullname'] ?></h2>
+        <h2><?php echo $_SESSION['role'] ?></h2>
 
         <?php footerScripts(); ?>
     </body>
--- index.php
+++ index.php
@@ -7,34 +7,43 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="es" data-mdb-theme="light">
+<html lang="es" data-mdb-theme="dark">
     <head>
         <?php headContent("Inicio de Sesion"); ?>
     </head>
     <body>
 
         <main>
-            <form class="card" action="login">
-                <div class="card-header text-center">
-                    Iniciar Sesion
-                </div>
-                <div class="card-body">
-                    <div class="form-outline mb-4" data-mdb-input-init>
-                        <input type="text" id="userlog" name="userlog" class="form-control" />
-                        <label class="form-label" for="userlog">Usuario:</label>
+            <form action="login">
+                <div class="card mb-4">
+                    <div class="card-body text-center">
+                        <h4 class="m-0">Iniciar Sesion</h4>
                     </div>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <div class="form-outline mb-4" data-mdb-input-init>
+                            <input type="text" id="userlog" name="userlog" class="form-control" />
+                            <label class="form-label" for="userlog">Usuario:</label>
+                        </div>
 
-                    <div class="form-outline" data-mdb-input-init>
-                        <input type="password" id="passlog" name="passlog" class="form-control" />
-                        <label class="form-label" for="passlog">Contraseña:</label>
+                        <div class="form-outline" data-mdb-input-init>
+                            <i class="far fa-eye" id="btnShowPass" data-input="#passlog"></i>
+                            <input type="password" id="passlog" name="passlog" class="form-control form-icon-trailing" />
+                            <label class="form-label" for="passlog">Contraseña:</label>
+                        </div>
                     </div>
                 </div>
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-primary btn-block">Enviar</button>
+                <div class="card">
+                    <div class="card-body">
+                        <button type="submit" class="btn btn-detail btn-block">
+                            <i class="fas fa-paper-plane"></i><span class="mx-2">Entrar</span><i class="fas fa-paper-plane"></i>
+                        </button>
+                    </div>
                 </div>
             </form>
+            <div class="index-icons" id="indexIcons" style="--icon-size: 50px;" data-icon-size="50"></div>
         </main>
-
         <?php footerScripts(); ?>
     </body>
 </html>
